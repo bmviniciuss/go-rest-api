468131ffc423fa5fe3cfd44d294196ccad7afbd9
bmviniciuss <bmvinicius11@gmail.com>
2021-07-17 01:05:45 -0300

    fix: Book entity path

--- database/migrations.go
+++ database/migrations.go
@@ -1,13 +1,13 @@
 package database
 
 import (
-	"github.com/bmvinicius/go-rest-api/models"
+	"github.com/bmvinicius/go-rest-api/src/books/domain/books"
 	"gorm.io/gorm"
 	"log"
 )
 
 func RunMigrations(db *gorm.DB) {
-	err := db.AutoMigrate(&models.Book{})
+	err := db.AutoMigrate(&books.Book{})
 	if err != nil {
 		log.Fatal(err)
 	}
